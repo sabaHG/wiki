36ada05e51e6a1809c0b312d70901a278dfc02e8
sabaHG <sabahg228@gmail.com>
2023-12-20 09:16:58 +0600

    first commit

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='home'),
     path('gallery/', views.gallery, name='gallery'),
     path('character/', views.character, name='character'),
-
+    path('news/', views.news_home, name='news_home'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,25 +1,44 @@
 from django.shortcuts import render,redirect
 from .models import Artiles
 from .forms import ArtilesForm
+from django.http import HttpResponseRedirect, HttpResponseBadRequest
+
 
 def news_home(request):
     news = Artiles.objects.all()
+    print(news)
     return render(request,'news/news_home.html', {'news': news})
 
 
 def add_news(request):
-    error = None
+    error = ''
     if request.method == 'POST':
-        form = ArtilesForm(request.POST)
+        form = ArtilesForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('news')
-    else:
-        error = 'error'
+        else:
 
-    form = ArtilesForm()
+            data = {
+                'form': form,
+                'error': 'Invalid form data. Please correct the errors below.',
+            }
+            return render(request, 'news/news_home.html', data)
+    else:
+        form = ArtilesForm()
+
     data = {
         'form': form,
-        'error': error
+        'error': error,
     }
+
     return render(request, 'news/add_news.html', data)
+
+
+def deletePers(request, id):
+    try:
+        character = Artiles.objects.get(id=id)
+        character.delete()
+        return redirect('news')
+    except Exception as error:
+        return HttpResponseBadRequest(error)
